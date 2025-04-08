e6e654933ba548d0fa7a4cc7086d207c7d3b1513
Pegu-Mrigen <mrig8520@gmail.com>
2025-04-08 10:20:39 +0530

    mng iss

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,7 +14,8 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+    {/* <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/"> */}
+    <ClerkProvider publishableKey="pk_test_ZmluZXItZG9iZXJtYW4tMTEuY2xlcmsuYWNjb3VudHMuZGV2JA" afterSignOutUrl="/">
       <AppContextProvider>
         <App />
       </AppContextProvider>
